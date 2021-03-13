8d70cfeb2d357d43e9ebbb0df96ab0bd0f5806f3
Abel Tran <abeltran.develop@gmail.com>
2021-03-13 12:21:29 +0700

    Add Google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -41,6 +41,17 @@ export default class CustomDocument extends Document {
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
                 integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
                 crossOrigin="anonymous"/>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-FDPZC2NF21"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-FDPZC2NF21')
+              `
+            }}
+          />
         </Head>
         <body>
         <Main/>
