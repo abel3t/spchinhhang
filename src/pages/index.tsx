31681d182fcc4f7f14d309c475980deae31294e4
Abel Tran <abeltran.develop@gmail.com>
2021-03-23 22:53:05 +0700

    Add ProductCard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import ProductCard from '../components/ProductCard';
 
 const IndexPage: React.FC<{}> = () => {
   return (
@@ -8,7 +9,17 @@ const IndexPage: React.FC<{}> = () => {
         <meta name="robots" content="noindex, nofollow"/>
         <meta name="viewport" content="viewport-fit=cover"/>
       </Head>
-      
+
+      <div className="flex flex-wrap -mx-4">
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+      </div>
 
     </>
   );
---
+++ src/pages/product/[slug].tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = () => {
+  return (
+    <>
+      <h1>Product</h1>
+    </>
+  );
+};
+
+export default Product;
