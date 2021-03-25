955c40835aaa1cf6fc5dd4b455210121b93b860d
Abel Tran <abeltran.develop@gmail.com>
2021-03-25 08:12:29 +0700

    Update

--- src/app-layouts/header/Header.tsx
+++ src/app-layouts/header/Header.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
-import Menu from './Menu';
+import React, { useState } from 'react';
+import AutoComplete from 'components/AutoComplete';
 
 export default function Header() {
+  const [ selectedOption, setSelectedOption ] = useState('');
+
   return (
     <>
-      <Menu/>
+      <nav className="py-2 md:py-4" style={{ backgroundColor: '#027FFF' }}>
+        <div className="container px-4 mx-auto md:flex md:items-center">
+          <a href="#" className="font-bold text-xl text-indigo-600">
+            <Logo/>
+          </a>
+
+          <AutoComplete options={[ 'Chennai', 'Mumbai', 'Bangalore' ]}
+                        value={selectedOption}
+                        onChange={setSelectedOption}
+          />
+
+          <a href="#"
+             className="p-2 lg:px-4 md:mx-2 text-white text-center border border-solid border-white rounded hover:bg-blue-700  hover:text-white transition-colors duration-300 mt-1 md:mt-0 md:ml-1">Liên
+            hệ quảng cáo </a>
+        </div>
+      </nav>
     </>
   );
 }
+
+const Logo = () => {
+  return (
+    <img
+      style={{ width: 100, height: 60, color: '#fff' }}
+      src="https://spchinhhang.s3-ap-southeast-1.amazonaws.com/logo.svg"
+      alt=" Workflow"
+    />
+  );
+};
--- src/components/AutoComplete.tsx
+++ src/components/AutoComplete.tsx
@@ -1,15 +1,102 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
-type AutoCompleteProps = {
-  placeholder: string
+export default function AutoComplete({ options, value, onChange }: any) {
+
+  const [ showOptions, setShowOptions ] = useState(false);
+  const [ cursor, setCursor ] = useState(-1);
+  const ref = useRef();
+
+  const select = (option: any) => {
+    onChange(option);
+    setShowOptions(false);
+  };
+
+  const handleChange = (text: string) => {
+    onChange(text);
+    setCursor(-1);
+    if (!showOptions) {
+      setShowOptions(true);
+    }
+  };
+
+  const filteredOptions = options.filter((option: any) => option.includes(value));
+
+  const moveCursorDown = () => {
+    if (cursor < filteredOptions.length - 1) {
+      setCursor(c => c + 1);
+    }
+  };
+
+  const moveCursorUp = () => {
+    if (cursor > 0) {
+      setCursor(c => c - 1);
+    }
+  };
+
+  const handleNav = (e: any) => {
+    switch (e.key) {
+      case 'ArrowUp':
+        moveCursorUp();
+        break;
+      case 'ArrowDown':
+        moveCursorDown();
+        break;
+      case 'Enter':
+        if (cursor >= 0 && cursor < filteredOptions.length) {
+          select(filteredOptions[cursor]);
+        }
+        break;
+    }
+  };
+
+  useEffect(() => {
+    const listener = (e: any) => {
+      if (!ref?.current.contains(e.target)) {
+        setShowOptions(false);
+        setCursor(-1);
+      }
+    };
+
+    document.addEventListener('click', listener);
+    document.addEventListener('focusin', listener);
+    return () => {
+      document.removeEventListener('click', listener);
+      document.removeEventListener('focusin', listener);
+    };
+  }, []);
+
+  return (<div className="relative w-64 " ref={ref}>
+
+    <input type="text" className="w-full border-2 px-4 py-2 outline-none rounded-lg"
+           value={value}
+           onChange={e => handleChange(e.target.value)}
+           onFocus={() => setShowOptions(true)}
+           onKeyDown={handleNav}
+    />
+
+    <ul className={`absolute w-full rounded-lg shadow-lg ${!showOptions && 'hidden'} select-none`}>
+      {filteredOptions.length > 0 ? filteredOptions.map((option: any, i: number, arr: []) => {
+        let className = 'px-4 hover:bg-gray-100';
+
+        if (i === 0)
+          className += 'pt-2 pb-1 rounded-t-lg';
+        else if (i === arr.length)
+          className += 'pt-1 pb-2 rounded-b-lg';
+        else if (i === 0 && arr.length)
+          className += 'py-2 rounded-lg';
+        else
+          className += 'py-1';
+
+        if (cursor === i) {
+          className += ' bg-gray-100';
+        }
+
+        return <li className={className}
+                   key={option}
+                   onClick={() => select(option)}
+        >{option}</li>;
+      }) : <li className="px-4 py-2 text-gray-500">No results</li>}
+
+    </ul>
+  </div>);
 }
-
-const AutoComplete = ({ placeholder }: AutoCompleteProps) => {
-  return (
-    <input type="text"
-           placeholder={placeholder}
-           className="form-input bg-teal-300 border border-blue-600 rounded w-full py-2 px-4 text-grey-800 leading-tight"/>
-  );
-};
-
-export default AutoComplete;
