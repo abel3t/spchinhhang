b53d856f84bbf8b70237019c23c1fe29a9315307
Abel Tran <abeltran.develop@gmail.com>
2021-04-19 21:45:17 +0700

    Add link

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Product = () => {
   return (
@@ -97,9 +98,14 @@ const Product = () => {
             </div>
             <div className="flex">
               <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
-              <button
-                className="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">Button
-              </button>
+
+              <Link
+                href="https://fast.accesstrade.com.vn/deep_link/5597000641894769237/4751584435713464237?url=https%3A%2F%2Fshopee.vn%2Fproduct%2F89827191%2F2265399926">
+                <button
+                  className="flex ml-auto text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded">Đặt
+                  Mua
+                </button>
+              </Link>
               <button
                 className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                 <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
