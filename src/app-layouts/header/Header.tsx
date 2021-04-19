b5af127492f5b9c3a6258c4897a0f6858dc5b712
Abel Tran <abeltran.develop@gmail.com>
2021-04-19 21:22:31 +0700

    Update

--- src/app-layouts/header/Header.tsx
+++ src/app-layouts/header/Header.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import AutoComplete from 'components/AutoComplete';
+import Category from '../../components/Category';
 
 export default function Header() {
   return (
@@ -11,19 +12,10 @@ export default function Header() {
                  src="https://spchinhhang.s3-ap-southeast-1.amazonaws.com/logo.svg" alt=""
                  width="100px"/>
           </a>
-          <ul
-            className="hidden absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:flex lg:items-center lg:w-auto lg:space-x-6">
-            <li><a className="text-sm text-white hover:text-gray-500" href="#">Start</a></li>
 
-            <li><a className="text-sm text-white font-bold" href="#">About Us</a></li>
+          <Category/>
 
-            <li><a className="text-sm text-white hover:text-gray-500" href="#">Services</a></li>
-
-            <li><a className="text-sm text-white hover:text-gray-500" href="#">Platform</a></li>
-
-            <li><a className="text-sm text-white hover:text-gray-500" href="#">Testimonials</a></li>
-          </ul>
-
+          <AutoComplete/>
           <a
             className="hidden lg:inline-block py-2 px-6 bg-primaryBold hover:bg-blue-700 text-sm text-white font-bold rounded-l-xl rounded-t-xl transition duration-200"
             href="#">Liên hệ quảng cáo</a>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { AppProps } from 'next/app';
 import AppLayout from '../app-layouts/AppLayout';
 
 import 'assets/main.css';
+import 'assets/index.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
